df2916c1c2b769aa8fd3a58fa3074456b22fefb9
Kian Farooghi <admin@Drive03-PC.Sedna.local>
2026-07-16 13:50:39 +0330

    Addind schemas to the project.

---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+# schema for creating a new item (a new Todo)
+class TodoCreate(BaseModel):
+    title: str
+    description: str
+    completed: bool
+
+
+# schema for creating modifying an old todo
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+
+class TodoResposnse(BaseModel):
+    id: int
+    title: str
+    description: str | None
+    completed: bool
